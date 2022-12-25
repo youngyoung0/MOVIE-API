7938fb4c2965121b3a70e0cf17bee2d068c6fbf2
youngyoung0 <taeyoung6873@gmail.com>
2022-12-26 00:44:46 +0900

    init

---
+++ moive/src/main/java/kofic/moive/MoiveApplication.java
@@ -0,0 +1,13 @@
+package kofic.moive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiveApplication.class, args);
+	}
+
+}
---
+++ moive/src/test/java/kofic/moive/MoiveApplicationTests.java
@@ -0,0 +1,13 @@
+package kofic.moive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
